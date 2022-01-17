8c3886cc7bba615b86f8bf3f6614e5592ce4a48c
Hugo Faye <hugo.faye@gmail.com>
2022-01-17 22:56:27 +0100

    add brevets route

--- controllers/events.js
+++ controllers/events.js
@@ -4,6 +4,16 @@ export async function getAll(req, res, next) {
   // const response = await fetch("https://veloenfrance.fr/ajax/json/events.json")
   fetch('https://veloenfrance.fr/ajax/json/events.json')
     .then(res => res.json())
-    .then(json => res.status(200).json(json));
+    .then(json => {
+      res.status(200).json(json)
+    });
 }
 
+export async function getAllBrevets(req, res, next) {
+  // const response = await fetch("https://veloenfrance.fr/ajax/json/events.json")
+  fetch('https://veloenfrance.fr/ajax/json/events.json')
+    .then(res => res.json())
+    .then(json => {
+      res.status(200).json(json.features.filter(feature => feature.properties.typenone.includes('Brevets')))
+    });
+}
--- routes/events.js
+++ routes/events.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 const router = Router();
 
-import { getAll } from '../controllers/events.js';
+import { getAll, getAllBrevets } from '../controllers/events.js';
 
 router.get('/', getAll);
+router.get('/brevets', getAllBrevets);
 
 export default router;
