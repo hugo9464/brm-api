cb857744bc10a8ea431d87081049e5da072a340a
Hugo Faye <hugo.faye@gmail.com>
2022-01-17 23:45:28 +0100

    join markers by coordinates

--- controllers/events.js
+++ controllers/events.js
@@ -1,19 +1,43 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 
 export async function getAll(req, res, next) {
   // const response = await fetch("https://veloenfrance.fr/ajax/json/events.json")
-  fetch('https://veloenfrance.fr/ajax/json/events.json')
-    .then(res => res.json())
-    .then(json => {
-      res.status(200).json(json)
+  fetch("https://veloenfrance.fr/ajax/json/events.json")
+    .then((res) => res.json())
+    .then((json) => {
+      res.status(200).json(json);
     });
 }
 
 export async function getAllBrevets(req, res, next) {
   // const response = await fetch("https://veloenfrance.fr/ajax/json/events.json")
-  fetch('https://veloenfrance.fr/ajax/json/events.json')
-    .then(res => res.json())
-    .then(json => {
-      res.status(200).json(json.features.filter(feature => feature.properties.typenone.includes('Brevets')))
+  fetch("https://veloenfrance.fr/ajax/json/events.json")
+    .then((res) => res.json())
+    .then((json) => {
+      var markers = [];
+      json.features
+        .filter(
+          (feature) =>
+            feature.properties.typenone.includes("Brevets") &&
+            feature.properties.dateyear === "2022"
+        )
+        .map((feature) => {
+          var marker = markers.find(
+            (marker) => JSON.stringify(marker.coordinates) === JSON.stringify(feature.geometry.coordinates)
+          );
+
+          if (!marker) {
+            var events = [];
+            events.push(feature.properties);
+            markers.push({
+              coordinates: feature.geometry.coordinates,
+              events: events,
+            });
+          } else {
+            marker.events.push(feature.properties)
+          }
+        });
+
+      res.status(200).json(markers);
     });
 }
